64faa17551c33809f5b1f13f3d5be6281e0b267a
Gabriel Paiva <mr.sousapaiva@gmail.com>
2025-08-14 14:28:27 +0100

    Refactor buttons to links and add new sections for subscription and design insights

--- index.php
+++ index.php
@@ -15,24 +15,46 @@
         <img class="w-full" src="./Assets/Imgs/hero_mobile.png">
         <h2 class="font-uni color-primary text-4xl">Tudo se encaixa</h2>
         <p class="font-switzer color-black text-base">Soluções modulares que se adaptam ao seu espaço, ao seu ritmo e à sua rotina. Uma nova forma de habitar com mais liberdade e menos excesso</p>
-        <button class="bg-primary">Ver Produtos</button>
-        <button class="bg-primary">Planear Espaço</button>
+        <a class="bg-primary">Ver Produtos</a>
+        <a class="bg-primary">Planear Espaço</a>
     </div>
 
     <div class="p-15 gap-10 bg-light">
         <h3 class="font-switzer color-primary text-xl">Moveis em destaque</h3>
         <p class="font-switzer color-black text-base">Funcionais, empilhaveis e prontos a encaixar</p>
-        <div>
+        <div class=""> <!-- Scrolable -->
 
         </div>
-        <button class="bg-primary">Ver todos os produtos</button>
+        <a class="bg-primary">Ver todos os produtos</a>
     </div>
 
     <div class="p-15 gap-10 bg-light">
         <h3 class="font-switzer color-primary text-xl">Planeie o seu espaço facilmente</h3>
         <p class="font-switzer color-black text-base">Use o nosso simulador para testar módulos no seu espaço. Ajuste, combine e visualize antes de comprar</p>
+        <img class="w-full" src="">
+        <a class="bg-primary">Começar a planear</a>
+    </div>
 
-        <button class="bg-primary">Começar a planear</button>
+    <div class="p-15 gap-10 bg-light">
+        <h3 class="font-switzer color-primary text-xl">Pensado para viver mais com menos</h3>
+        <p class="font-switzer color-black text-base">A Komodu nasceu para simplificar a vida urbana. Criamos um catalogo de móveis modulares, funcionais e flexiveis, que crescem consigo e se adptam ao seu espaço, e não o contrário.</p>
+        <a class="bg-primary">Saber mais sobre a Komudo</a>
+    </div>
+
+    <div class="p-15 gap-10 bg-light">
+        <h3 class="font-switzer color-primary text-xl">Não perca nenhuma adição</h3>
+        <p class="font-switzer color-black text-base">Dicas, inspirações e novidades sobre como viver bem em menos espaço</p>
+        <form>
+            <input type="text" placeholder="email.exemplo@gmail.com">
+            <input type="submit" class="bg-primary">Subscrever</button>
+        </form>
+    </div>
+
+    <div class="p-15 gap-10 bg-light">
+        <h3 class="font-switzer color-primary text-xl">Os nossos espaços</h3>
+        <p class="font-switzer color-black text-base">Uma visão da Komudu sobre como o espaço urbano pode ser mais flexível, funcional e bonito. Estes ambientes representam a essência do nosso design modular.</p>
+        <img class="w-full" src="">
+        <p class="font-switzer color-black opacity-60 text-sm">“O espaço nao precisa de ser grande. Só precisa de fazer sentido”</p>
     </div>
 </body>
 </html>
